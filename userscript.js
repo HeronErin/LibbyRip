bd5a6b699e83b9955e9170252087eeed0d3567cc
Elanor <oranges13@users.noreply.github.com>
2025-02-20 11:37:04 -0500

    Update userscript.js Version

--- userscript.js
+++ userscript.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name          LibreGRAB
 // @namespace     http://tampermonkey.net/
-// @version       2025-02-02
+// @version       2025-02-20
 // @description   Download all the booty!
 // @author        HeronErin
 // @license       MIT
